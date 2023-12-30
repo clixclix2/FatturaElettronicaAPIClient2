15ac087929d4e8795d1fe45d187433aa90ae2200
Claudio C <c.clix@tiscali.it>
2023-12-30 21:00:35 +0100

    Create paginazione.php

---
+++ esempi/paginazione.php
@@ -0,0 +1,21 @@
+<?php
+// esempio di gestione della paginazione (per volumi alti, sopra ai 1000 risultati attesi per chiamata)
+require_once 'FatturaElettronicaApiClient2.class.php';
+$feac = new FatturaElettronicaApiClient2($username, $password);
+$res = $feac->ricevi();
+if ($res) {
+    $hasData = true;
+    while ($hasData) {
+        foreach ($res as $arrDati) {
+            if ($arrDati['ricezione']) {
+                // ricezione di un documento
+            } else {
+                // ricezione di un aggiornamento di trasmissione
+            }
+        }
+        if ($feac->hasMoreResults()) {
+            $res = $feac->getNextResults();
+            $hasData = true;
+        }
+    }
+}
