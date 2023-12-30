96fa797d6565c83ae571849c28b20416feb63a36
Claudio C <c.clix@tiscali.it>
2023-12-30 21:09:17 +0100

    Create multi-azienda.php

---
+++ esempi/multi-azienda.php
@@ -0,0 +1,74 @@
+<?php
+
+// Esempi di gestione multi-azienda
+
+require_once 'FatturaElettronicaApiClient2.class.php';
+
+$username = '........'; //Username e password forniti dal servizio
+$password = '........';
+$feac = new FatturaElettronicaApiClient2($username, $password);
+
+
+$tipoTest = 'elenco';
+
+
+if ($tipoTest == 'elenco') {
+
+    $res = $feac->elencoAziende();
+
+    if (!$res) {
+        echo($feac->getLastError());
+    } else {
+        foreach ($res as $azienda) {
+            var_dump($azienda);
+        }
+    }
+}
+
+
+if ($tipoTest == 'aggiungi') {
+
+    $ragioneSociale = 'Azienda Test Srl';
+    $partitaIva = '12345678901';
+    $codiceFiscale = '12345678901';
+
+    $res = $feac->aggiungiAzienda(array(
+        'ragione_sociale' => $ragioneSociale,
+        'piva' => $partitaIva,
+        'cfis' => $codiceFiscale
+    ));
+
+    if ($res) {
+        $idAzienda = $res['id'];
+    } else {
+        echo($feac->getLastError());
+    }
+
+}
+
+if ($tipoTest == 'dati') {
+
+    $idAzienda = 100;
+
+    $res = $feac->datiAzienda($idAzienda);
+
+    if (!$res) {
+        echo($feac->getLastError());
+    } else {
+        var_dump($res);
+    }
+}
+
+
+if ($tipoTest == 'rimuovi') {
+
+    $idAzienda = 100;
+
+    $res = $feac->rimuoviAzienda($idAzienda);
+
+    if (!$res) {
+        echo($feac->getLastError());
+    }
+}
+
+
